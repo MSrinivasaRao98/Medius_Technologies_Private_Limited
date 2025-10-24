a761b59155493285c28f0e7bd85c1d2d6488c169
SrinivasaRao98 <srinivas158.sc@gmail.com>
2025-10-25 01:56:53 +0530

    push the Medius Technologies Private Limited to master branch

---
+++ Medius_Technologies_Private_Limited/src/main/java/google_Form/Medius_Technologies_Private_Limited/App.java
@@ -0,0 +1,10 @@
+package google_Form.Medius_Technologies_Private_Limited;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Medius_Technologies_Private_Limited/src/test/java/google_Form/Medius_Technologies_Private_Limited/AppTest.java
@@ -0,0 +1,19 @@
+package google_Form.Medius_Technologies_Private_Limited;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
